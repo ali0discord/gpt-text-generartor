3479282d96b785d63a78257bb6dc1d3c3781693e
Alireza <alirzahosinopoor.1390@gmail.com>
2024-12-12 12:10:46 +0330

    add .sefetensors format support

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import os
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, CodeGenForCausalLM, AutoTokenizer
 
 def load_model(model_name):
@@ -23,10 +24,12 @@ def load_model(model_name):
         tokenizer = AutoTokenizer.from_pretrained("./models/codegen")
     elif model_name == "gpt2-large":
         print("Loading GPT2 Large Model...")
-        model = GPT2LMHeadModel.from_pretrained("./models/gpt2-large")
-        tokenizer = GPT2Tokenizer.from_pretrained("./models/gpt2-large")
-    else:
-        raise ValueError(f"Unsupported model name: {model_name}")
+        if os.path.exists(os.path.join("./models/gpt2-large", "pytorch_model.bin")):
+            model = GPT2LMHeadModel.from_pretrained("./models/gpt2-large")
+        elif os.path.exists(os.path.join("./models/gpt2-large", "model.safetensors")):
+            model = GPT2LMHeadModel.from_pretrained("./models/gpt2-large", from_tf=False, trust_remote_code=True)
+        else:
+            raise FileNotFoundError("Neither 'pytorch_model.bin' nor 'model.safetensors' found in the specified path.")
 
     # Configure the pad_token for text models
     if tokenizer:
